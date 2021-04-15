aea1d87204fa471e7dd2e57a4da721bc6b821c94
Harsh Singh <harshsin@usc.edu>
2021-04-14 21:43:32 -0700

    initial stuff

---
+++ src/main/java/dev/harshsingh/spring/sfgdi/controllers/MyController.java
@@ -0,0 +1,12 @@
+package dev.harshsingh.spring.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("Hello World!");
+
+        return "Hi Folks!";
+    }
+}
